0e0f13dc2567780a11a9a8af9e60dd0445872fe3
rahayuningh <rahayuning_hardatin@apps.ipb.ac.id>
2020-03-12 23:29:45 +0700

    Templating

---
+++ resources/views/layouts/css2.blade.php
@@ -0,0 +1,9 @@
+<link href="{{ asset('css/bootstrap.min.css')}}" rel="stylesheet">
+<link href="{{ asset('css/bootstrap-responsive.min.css')}}" rel="stylesheet">
+
+<link href="http://fonts.googleapis.com/css?family=Open+Sans:400italic,600italic,400,600" rel="stylesheet">
+<link href="{{ asset('css/font-awesome.css') }}" rel="stylesheet">
+
+<link href="{{ asset('css/style.css') }}" rel="stylesheet">
+
+<link href="{{ asset('css/pages/reports.css') }}" rel="stylesheet">
--- routes/web.php
+++ routes/web.php
@@ -14,8 +14,45 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
+    return view('dashboard');
+});
+
+Route::get('/welcome', function () {
     return view('welcome');
 });
+
+Route::get('/reports', function () {
+    return view('reports');
+});
+Route::get('/guidely', function () {
+    return view('guidely');
+});
+Route::get('/charts', function () {
+    return view('charts');
+});
+Route::get('/shortcodes', function () {
+    return view('shortcodes');
+});
+
+Route::get('/icons', function () {
+    return view('icons');
+});
+Route::get('/faq', function () {
+    return view('faq');
+});
+Route::get('/pricing', function () {
+    return view('pricing');
+});
+Route::get('/login', function () {
+    return view('login');
+});
+Route::get('/signup', function () {
+    return view('signup');
+});
+Route::get('/error', function () {
+    return view('error');
+});
+
 
 Auth::routes();
 
