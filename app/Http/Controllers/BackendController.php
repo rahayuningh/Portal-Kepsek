025399be404179569c3dc4cd68ac399fe2abff8a
Muhammad Fakhri <muhammad_fakhri@apps.ipb.ac.id>
2020-04-27 22:21:53 +0700

    fill BackendController with function for generate data siswa and civitas using polymorphic relation

---
+++ app/Http/Controllers/BackendController.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Civitas;
+use App\Siswa;
+use Carbon\Carbon;
+use Illuminate\Http\Request;
+
+class BackendController extends Controller
+{
+    public function createCivitas()
+    {
+        $currentDate = Carbon::today();
+        $civitas = new Civitas(
+            [
+                'nama' => 'Muhammad Fakhri',
+                'jenis_kelamin' => True,
+                'tempat_lahir' => 'Bogor, Jawa Barat',
+                'tanggal_lahir' => $currentDate,
+                'agama' => 1,
+            ]
+        );
+
+        $siswa = Siswa::create([
+            'nisn' => 'G64170015',
+            'asal_wilayah' => 'Jakarta Utara',
+            'id_kelas_1' => 1,
+            'id_kelas_2' => 2,
+            'id_kelas_3' => 3,
+            'status_keaktifan' => 1
+        ]);
+
+        $data = $siswa->civitas()->save($civitas);
+
+        echo "nice";
+    }
+
+    public function seeSiswa($id)
+    {
+        $siswa = Siswa::find($id);
+        $civitas = $siswa->civitas;
+        // echo($civitas);
+        echo($siswa);
+    }
+}
