14f0f81e5ffbd08f4d18cce35a3fd7cc2e6bade2
Muhammad Fakhri <muhammad_fakhri@apps.ipb.ac.id>
2020-04-27 22:22:52 +0700

    Add some column to fillable for mass assignment

--- app/Civitas.php
+++ app/Civitas.php
@@ -6,6 +6,14 @@ use Illuminate\Database\Eloquent\Model;
 
 class Civitas extends Model
 {
+    protected $fillable = [
+        'nama',
+        'jenis_kelamin',
+        'tempat_lahir',
+        'tanggal_lahir',
+        'agama'
+    ];
+
     public function civitasable()
     {
         return $this->morphTo();
--- app/Siswa.php
+++ app/Siswa.php
@@ -7,6 +7,15 @@ use Illuminate\Database\Eloquent\Relations\MorphMany;
 
 class Siswa extends Model
 {
+    protected $fillable = [
+        'nisn',
+        'asal_wilayah',
+        'id_kelas_1',
+        'id_kelas_2',
+        'id_kelas_3',
+        'status_keaktifan'
+    ];
+
     public function civitas()
     {
         return $this->morphMany('App\Civitas', 'civitasable');
