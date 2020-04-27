64d982edb6433f65d8c40917dfecd8856f347183
Muhammad Fakhri <muhammad_fakhri@apps.ipb.ac.id>
2020-04-27 21:40:46 +0700

    Add seeder for Civitas, Guru, Pegawai, Siswa, and Tendik

---
+++ database/seeds/CivitasTableSeeder.php
@@ -0,0 +1,16 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class CivitasTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+    }
+}
