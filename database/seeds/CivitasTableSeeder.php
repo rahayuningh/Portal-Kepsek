75cd75c584f4efc497747137e60be16a36d09ee3
Muhammad Fakhri <muhammad_fakhri@apps.ipb.ac.id>
2020-04-27 21:44:32 +0700

    generate new controller for backend developing purpose

--- database/seeds/CivitasTableSeeder.php
+++ database/seeds/CivitasTableSeeder.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
 
 class CivitasTableSeeder extends Seeder
 {
@@ -11,6 +13,29 @@ class CivitasTableSeeder extends Seeder
      */
     public function run()
     {
-        //
+        DB::table('civitas')->insert([
+            'nama' => 'John Doe',
+            'nisn' => '9991234567',
+            'asal_wilayah' => 'Dramaga',
+            'id_kelas_1' => '11',
+            'id_kelas_2' => '21',
+            'id_kelas_3' => '31',
+            'tempat_lahir' => 'Dramaga',
+            'tanggal_lahir' => '1 Januari 2002',
+            'agama' => 'Islam',
+            'jenis_kelamin' => 'Laki-laki',
+        ],[
+            'nama_siswa' => 'Jane Doe',
+            'nisn' => '0001234567',
+            'asal_wilayah' => 'Jakarta',
+            'id_kelas_1' => '12',
+            'id_kelas_2' => '22',
+            'id_kelas_3' => '32',
+            'tempat_lahir' => 'Jakarta',
+            'tanggal_lahir' => '31 Januari 2002',
+            'agama' => 'Kristen',
+            'jenis_kelamin' => 'Perempuan',
+        ]
+    );
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -17,6 +17,10 @@ Route::get('/', function () {
     return view('index');
 });
 
+Route::get('/', function () {
+    return view('index');
+});
+
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
