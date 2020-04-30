b1410ea943e0933dddfde8ab3c31cd5db8ba478e
rahayuningh <rahayuning_hardatin@apps.ipb.ac.id>
2020-04-30 10:02:47 +0700

    Update Tampilan

--- routes/web.php
+++ routes/web.php
@@ -17,7 +17,6 @@ Route::get('/', function () {
     return view('Beranda');
 });
 
-<<<<<<< HEAD
 // BERANDA
 Route::get('/home', function () {
     return view('Beranda');
@@ -74,14 +73,13 @@ Route::get('/reset', function () {
 Route::get('/confirm-reset', function () {
     return view('auth/passwords/confirm');
 });
-=======
+
 // Route made by Fakhri
 // If you want to know what is it for, ask Fakhri :)
 Route::get('/civitas', 'BackendController@createCivitas');
 Route::get('/siswa/{id}', 'BackendController@seeSiswa');
 // -----------------
 
->>>>>>> 132fd931b675af71f0fed91ecb98873e44777187
 Auth::routes();
 
 
